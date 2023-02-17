ed67521be60fe567db962b4eef49bf9f8f2d1115
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 20:25:48 +0000

    b01- step4-with quotation

---
+++ basic1/step4.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Gaby';
+$email = 'info@gabykaram.com';
+$position="tech consultant";
+$github_url="https://github.com/gk-git";
+
+// Output
+echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}";
+?>
