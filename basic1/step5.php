84ce4b0151debf4c13b2deea3d8fc2e08e8ef61a
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 20:51:39 +0000

    b01- step5-option1

---
+++ basic1/step5.php
@@ -0,0 +1,14 @@
+<?php
+
+$course_name="Rust";
+$enrolled_students=20;
+$price=30;
+$on_discount=true;
+
+
+echo "Course title: ${course_name}, the language of no bugs\n";
+echo "Enrolled Students: ${enrolled_students}\n";
+echo "Course price: ${price} $ USD\n";
+echo "Course on discount: " . ($on_discount ? "Yes" : "No") . "\n";
+
+?>
