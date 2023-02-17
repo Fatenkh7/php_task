3232305755d3611acc8963ddd931a36373c67c49
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 20:55:21 +0000

    b01- step5-option3

--- basic1/step5.php
+++ basic1/step5.php
@@ -1,17 +1,14 @@
 <?php
 
-$course_name = "Rust, the language of no bugs";
-$enrolled_students = 20;
-$price = 30.0;
-$on_discount = true;
+$course_name="Rust";
+$enrolled_students=20;
+$price=30;
+$on_discount=true;
 
-echo "Course title: \"$course_name\"\n";
-echo "Enrolled Students: $enrolled_students\n";
-echo "Course price: $price $ USD\n";
 
-if ($on_discount) {
-    echo "Course on discount: Yes\n";
-} else {
-    echo "Course on discount: No\n";
-}
+echo "Course title: " . $course_name . ", the language of no bugs\n";
+echo "Enrolled Students: " . $enrolled_students . "\n";
+echo "Course price: " . $price . " $ USD\n";
+echo "Course on discount: " . ($on_discount ? "Yes" : "No") . "\n";
+
 ?>
