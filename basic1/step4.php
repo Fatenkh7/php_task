5cd3331d2d98aff90192247ceb9ca155a62f6565
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 20:36:15 +0000

    b01- step4-with dot

--- basic1/step4.php
+++ basic1/step4.php
@@ -1,9 +1,9 @@
 <?php
 $name = 'Gaby';
 $email = 'info@gabykaram.com';
-$position="tech consultant";
-$github_url="https://github.com/gk-git";
+$position = "tech consultant";
+$github_url = "https://github.com/gk-git";
 
 // Output
-echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}";
+echo "Hello, I'm ".$name.", I'm a ".$position.". Please check my Github link: ".$github_url;
 ?>
