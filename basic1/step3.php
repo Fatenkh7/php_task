b11a85e3ad74fc299aa89c7fc4dae613a7afab21
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 20:07:40 +0000

    b01- step3-adding the area

--- basic1/step3.php
+++ basic1/step3.php
@@ -1,7 +1,11 @@
 <?php
 $name = 'Gaby';
 $email = 'info@gabykaram.com';
+$height=180;
+$width=75;
+$area=$height+$width;
 
 // Output
-echo "My name is ${name} and my email is ${email}";
+echo "My name is ${name} and my email is ${email}\n";
+echo "The area is ${area}"
 ?>
