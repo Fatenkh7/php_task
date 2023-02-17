7316aa532718625c3b60d85cead659141b99c5b6
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 21:27:23 +0000

    b01- step8-indexed arr

---
+++ basic1/step8.php
@@ -0,0 +1,6 @@
+<?php
+
+$myArray = ["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+
+echo "Hello Sir, do you have {$myArray[0]}, {$myArray[1]}, and {$myArray[2]}? Also, if you sell fruits, can I find a {$myArray[count($myArray) - 1]}?";
+?>
