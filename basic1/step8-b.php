38b2d955361c6e83214053c43973d1f2361c7a7a
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 21:36:11 +0000

    b01- step8-b

---
+++ basic1/step8-b.php
@@ -0,0 +1,10 @@
+<?php
+
+$myArray = [
+  'eggs' => ['balade', 'mazere3'],
+  'milk' => ['Fresh', 'Taanayel'],
+  'water-pack' => ['Tanoureen', 'Reem']
+];
+
+echo "Hey Sir, Please I need 1 pack of {$myArray['eggs'][0]} eggs and 3 {$myArray['water-pack'][1]} Water Pack.";
+?>
