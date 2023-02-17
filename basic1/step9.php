3545cdb7ee62bd48658c411e667d8bc8bc06a58d
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-17 21:44:17 +0000

    b01- step9

---
+++ basic1/step9.php
@@ -0,0 +1,13 @@
+<?php
+
+$name="Faten";
+$num=9.097;
+$active=true;
+$myArray = ["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+
+var_dump($name);
+var_dump($myArray);
+var_dump($num);
+var_dump($active);
+
+?>
