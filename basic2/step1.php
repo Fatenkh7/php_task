5b68ae08dd1294c93ae4160ebe7da3b5baa8376a
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-18 19:16:02 +0000

    b02- step1

---
+++ basic2/step1.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang="PHP";
+$course_name="global variables";
+$command="sudo rm -rf /";
+
+
+echo "Tomorrow I 'll learn " . $lang ." ".$course_name. "\n This is a bad command: ".$command;
+?>
